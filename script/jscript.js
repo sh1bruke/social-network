e744df39db6920d7f9d464451fad4c5a9aa59199
gaby2057 <gaby2057@hotmail.com>
2019-05-29 15:41:18 -0300

    modificaciones varias

--- script/jscript.js
+++ script/jscript.js
@@ -1,10 +1,32 @@
-$(document).ready(function(){
-    var placeholder = null;
-    $('input[type=text]').focus(function(){
-      placeholder = $(this).attr("placeholder");
-      $(this).attr("placeholder","");
-    });
-    $('input[type=text]').blur(function(){
-      $(this).attr("placeholder", placeholder);
-    });
-  });
+  function crearCuenta(){ 
+    //valido el nombre 
+    if (document.formulario.nombre.value.length==0){ 
+       alert("Tiene que escribir su nombre") 
+       document.formulario.nombre.focus() 
+       return 0; 
+    } 
+
+    if (document.formulario.correo.value.length==0){ 
+      alert("Tiene que escribir su correo") 
+      document.formulario.nombre.focus() 
+      return 0; 
+    }
+
+    if (document.formulario.usuario.value.length==0){ 
+    alert("Tiene que escribir su usuario") 
+    document.formulario.nombre.focus() 
+    return 0; 
+    }
+
+    if (document.formulario.pass.value.length==0){ 
+    alert("Tiene que repetir su contraseña") 
+    document.formulario.nombre.focus() 
+    return 0; 
+    }
+     
+    
+    //el formulario se envia 
+    if (formulario.usuario.correo.nombre.value.length!=0){
+    alert("Muchas gracias por enviar el formulario"); 
+    document.formulario.submit(); }
+}//falta terminar el aviso de gracias por enviar******************
